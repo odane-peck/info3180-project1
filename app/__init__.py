7e1c463b48b012183801613d5ce252e5d0459450
Odane E Peck <odane.peck@mymona.uwi.edu>
2017-05-19 07:05:30 +0000

    g

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,15 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "SUPERCALIFRAGILISTICSECRETKEY"
 #app.config['SQLALCHEMY_DATABASE_URI'] = "ubuntu://user:password@localhost/projecto1"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://ubuntu:password-01@localhost/CAPSTONE'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 app.debug = True
-
+app.config['SECRET_KEY'] = 'super secret key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/mydatabase.db'
 db = SQLAlchemy(app)
 CSRF_ENABLED = True
 from app import models
